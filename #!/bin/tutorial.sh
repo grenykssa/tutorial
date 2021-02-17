c48b622d2e771f5d058a5288d5cac62130abb76a
grenykssa <79082904+grenykssa@users.noreply.github.com>
2021-02-17 10:36:58 +0700

    Create tutorial.sh

---
+++ #!/bin/tutorial.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+green='\33[32;1m'
+blue='\33[0;36m'
+red='\33[31;1m'
+purple='\e[0;35m'
+Oren='\33[33;1m'
+black='\33[30;1m'
+white='\33[37;1m'
+yellow='\33[33;1m'
+
+clear
+
+toilet -f big -F gay fauzan ck
+echo
+sleep 2
+clear
+
+echo "\33[32;1m   █████████ " | lolcat
+echo "\33[32;1m   █▄█████▄█ " | lolcat
+echo "\33[32;1m   █▼▼▼▼▼    " | lolcat
+echo "\33[32;1m   █ WELCOME    " | lolcat
+echo "\33[32;1m   █▲▲▲▲▲    " | lolcat
+echo "\33[32;1m   █████████ " | lolcat
+echo "\33[32;1m   __██____██___" | lolcat
+echo 
+sleep 2
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+toilet -f big -F gay password
+echo "\033[33;1m ××××××××××××××××××××××××××××××"
+echo "\033[33;1m MASUKAN PASSWORD : GAZINA07"
+echo "\033[33;1m ××××××××××××××××××××××××××××××"
+
+name="GAZINA07"
+while [ "$adin" != "$name" ]
+do
+     read -p "MASUKAN PASSWORD :)" adin
+done
+  echo "\033[32;1m [✓] SUCCESS..."
+  sleep 2
+  clear   
+
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+
+echo "\033[32;1m  ++++==============++++"
+echo "\033[32;1m      MENU"
+echo "\033[32;1m  ++++==============++++"
+echo "\033[31;1m   (01) KUMPULAN BUG"
+echo "\033[31;1m   (02) STABILKAN JARINGAN"
+echo "\033[37;1m MASUKAN NOMOR MENU"
+read -p " [ PILIH MENU ]>>>" pil ;
+if [ $pil = "01" ]
+then
+clear
+toilet -f big -F gay welcome
+
+echo "\033[31;1m    =============MENU BUG=============="
+echo "\033[31;1m      (01) BUG TSEL MAXSTREAM"
+echo "\033[31;1m      (02) BUG TSEL WHATSAPP"
+echo "\033[31;1m      (03) BUG TSEL INSTAGRAM"
+echo "\033[31;1m      (04) BUG TSEL YOUTUBE"
+echo "\033[31;1m      (05) BUG TSEL KB BIASA"
+echo "\033[31;1m      (06) BUG TSEL UNLIMAX"
+echo "\033[31;1m      (07) EXIT..."
+echo "\033[31;1m    ===================================="
+echo "\033[33;1m    PILIH MENU
+read -p " [ PILIH NOMOR ]>>>" pilih ;
+if [ $pilih = "01" ]
+then 
+clear
+toilet -f big -F gay bug maxstream
+echo "\033[32;1m Loading..."
+sleep 2
+clear
+echo "\033[37;1m ==============="
+echo "\033[37;1m play.hbogo.eu (SNI)"
+echo "\033[37;1m ==============="
+sleep 2
+echo "\033[37;1m ++++BUG MAXSTREAM++++"
+fi
+if [ $pil = "02" ]
+then
+clear
+toilet -f big -F stabil ping
+echo "\033[32;1m Loading..."
+sleep 2
+clear
+pkg update && pkg upgrade -y
+      ping -s 1000 8.8.8.8
+      fi
